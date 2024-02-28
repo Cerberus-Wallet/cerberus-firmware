37bf9458b45a6e867191871c7242188424341e32
matejcik <ja@matejcik.cz>
2024-02-28 10:04:15 +0100

    feat(core): expose flash area API to Python
    and add debuglink messages for reading/writing flash
    
    this also refactors get_firmware_hash to use the flash area API instead of the original hardcoded function

---
+++ core/mocks/generated/trezorio/flash_area.pyi
@@ -0,0 +1,9 @@
+from typing import *
+from . import FlashArea
+BOARDLOADER: FlashArea
+BOOTLOADER: FlashArea
+FIRMWARE: FlashArea
+STORAGE_A: FlashArea
+STORAGE_B: FlashArea
+TRANSLATIONS: FlashArea
+SECRET: FlashArea
